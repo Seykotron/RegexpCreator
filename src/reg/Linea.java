98a0e5ddab29b74d616b41a868863190e671d83a
Miguel Angel Romero de los Llanos <seykotron@gmail.com>
2016-12-19 13:47:17 +0100

    Initial Commit

---
+++ src/reg/Linea.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reg;
+
+/**
+ *
+ * @author Miguel Angel
+ */
+public class Linea {
+    
+    private int numero;
+    private String texto;
+
+    public Linea(int numero, String texto) {
+        this.numero = numero;
+        this.texto = texto;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    
+    
+    
+}
